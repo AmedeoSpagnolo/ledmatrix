4bb723b1303c8cce349671397c645573432c8a01
Amedeo SPAGNOLO <amedeo.spagnolo@gmail.com>
2017-10-04 12:24:04 +0800

    function delete empty column

--- char_converter/leds.js
+++ char_converter/leds.js
@@ -1,15 +1,18 @@
-var row = 16
-var column = 24
-var matrix = Array(column).fill(Array(row))
-// matrix[0][1] = 1
-// console.log(matrix[0]);
-// console.log(matrix[1]);
+var app = {}
+app.leds = {}
 
+app.leds.row = 16
+app.leds.column = 24
+app.leds.matrix = matrix = new Array(app.leds.column);
+for (var i = 0; i < app.leds.column; i++) {
+  matrix[i] = new Array(app.leds.row).fill(0);
+}
 
 ;$( document ).ready(function() {
   $("svg").on("click", "circle", function(){
     toggleled(this)
   })
+  $("#value").text(get_value_from_matrix())
 })
 
 function toggleled(element){
@@ -24,16 +27,31 @@ function toggleled(element){
 }
 
 function get_value_from_matrix(){
-  // first number: max column
+
+  matrix[0][15] = 1
+  matrix[5][13] = 1
+
   // convert binary every column
   var temp = []
   matrix.forEach(function(i){
     var bin = i.join("")
     temp.push(parseInt(bin, 2))
   })
-  return "uint8_t newChar[] = { " + temp.join(", ") + " } // rename newChar"
+  var arr = delete_empty_column(temp)
+  return "uint8_t newChar[] = { " + arr.join(", ") + " } // rename newChar"
 }
 
 function update_matrix(){
   // get value from svg
+  // TO DO
+}
+
+function delete_empty_column(ar){
+  for (var i = ar.length - 1; i != 0; i--){
+    if (parseInt(ar[i],2) == 0 ) {
+      ar.splice(i)
+    } else {
+      return ar
+    }
+  }
 }
