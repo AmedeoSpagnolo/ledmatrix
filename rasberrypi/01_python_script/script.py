a884a099cb1e03ceffc05d1de9f78d34fc5e22f8
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2017-12-10 13:22:48 +0800

    update boris led

--- rasberrypi/01_python_script/script.py
+++ rasberrypi/01_python_script/script.py
@@ -1,28 +1,90 @@
 #!/usr/bin/env python
 from samplebase import SampleBase
-
+from rgbmatrix import graphics
+import time
 
 class SimpleSquare(SampleBase):
     def __init__(self, *args, **kwargs):
         super(SimpleSquare, self).__init__(*args, **kwargs)
 
-    def pixel(self,canvas,x,y,r,g,b):
-        canvas.SetPixel(x,y,r,g,b)
+    # SetPixel
+    # canvas.SetPixel(x,y,r,g,b)
 
-    def line(self,canvas,x,y,w,r,g,b):
-        for i in range(0,w):
-            canvas.SetPixel(x+i,y,r,g,b)
+    # DrawLine
+    # graphics.DrawLine(canvas, x0, y0, x1, y1, graphics.Color(255, 0, 0))
+
+    # DrawCircle
+    # graphics.DrawCircle(canvas, cx, cy, r, graphics.Color(255, 0, 0))
+
+    # DrawText
+    # graphics.DrawText(canvas, font, 2, 10, blue, "Text")
+
+    # Fill
+    # self.matrix.Fill(c, 0, 0)
 
     def run(self):
-        offset_canvas = self.matrix.CreateFrameCanvas()
+        canvas = self.matrix.CreateFrameCanvas()
+
+        red     = graphics.Color(255, 0, 0)
+        green   = graphics.Color(0, 255, 0)
+        blue    = graphics.Color(0, 0, 255)
+        white   = graphics.Color(255,255,255)
+        font    = graphics.Font()
+        font.LoadFont("fonts/4x6.bdf")
+
+        # position
+        # pos = offscreen_canvas.width
+
+        # brightness
+        max_brightness = self.matrix.brightness
+
+        loop = [
+          "man",
+          "design",
+          "branding",
+          "advertising",
+          "typography",
+          "photography",
+          "illustration",
+          "editorial",
+          "video",
+          "print",
+          "web"
+        ]
+
+        loop_temp = [
+          "ma",
+          "d",
+          "b",
+          "adr",
+          "ty",
+          "p",
+          "ilr",
+          "ed4",
+          "v",
+          "pr",
+          "w"
+        ]
+
+        count       = 0
+        marginleft  = 2
+        margintop   = 10
+
+        def ll(string):
+            return sum([font.CharacterWidth(ord(c)) for c in string])
+
         while True:
+            graphics.DrawText(canvas, font, marginleft, margintop, white, "FELD")
+            graphics.DrawText(canvas, font, marginleft + ll("FELD"), margintop, white, loop_temp[count])
+            graphics.DrawLine(canvas, marginleft + ll("FELD"), margintop + 1, marginleft + ll(loop_temp[count]+"FELD"), margintop + 1, white)
+            canvas.SetPixel(marginleft + ll(loop_temp[count]+"FELD"), margintop - 1,255,255,255)
+            time.sleep(1)
 
-            # some amazing code
-            self.line(offset_canvas,0,0,10,0,255,0)
-            self.line(offset_canvas,0,2,32,0,255,0)
-            self.line(offset_canvas,16,15,8,0,50,255)
+            count += 1
+            count = count % len(loop_temp)
 
-            offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
+            canvas = self.matrix.SwapOnVSync(canvas)
+            canvas.Clear()
 
 
 # Main function
@@ -30,3 +92,6 @@ if __name__ == "__main__":
     simple_square = SimpleSquare()
     if (not simple_square.process()):
         simple_square.print_help()
+
+# due led matrix
+# animazione
